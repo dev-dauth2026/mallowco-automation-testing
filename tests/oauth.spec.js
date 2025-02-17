79c7cac57c9a466812c91e5d28dc4721a37be33e
Dauth Gurung <dauth22grg22@gmail.com>
2025-02-17 14:43:33 +1000

    import name changed

--- tests/oauth.spec.js
+++ tests/oauth.spec.js
@@ -1,39 +1,39 @@
 import {test,expect} from '@playwright/test'
-import { Auth } from '../pages/Login'
+import { Login } from '../pages/Login'
 import 'dotenv/config';
 
 test.describe('User Authentication Test', ()=>{
     let auth;
 
     test.beforeEach('Navigate to Home Page ',async({page})=>{
-        auth =  new Auth(page);
+        login =  new Login(page);
 
         // Navigate to the home page
-        await auth.goHomepage();
+        await login.goHomepage();
     });
 
     test('User login successful',async ({page})=>{
         
         // Perform login with valid credentials
-        await auth.login(process.env.EMAIL, process.env.PASSWORD);
+        await login.login(process.env.EMAIL, process.env.PASSWORD);
 
         //Wait for the page to handle login 
         await page.waitForTimeout(2000);
 
-        const isSuccessful = await auth.isLoginSuccessful();
+        const isSuccessful = await login.isLoginSuccessful();
         expect(isSuccessful).toBe(true);
     });
 
     test('User login fail',async({page})=>{
         
         // Perform login with invalid credentials
-        await auth.login('david2020g@gmail.com','dav1234#')
+        await login.login('david2020g@gmail.com','dav1234#')
         
         // Wait for the page to handle login
         await page.waitForTimeout(2000);
 
-        const isSuccessful = await auth.isLoginSuccessful();
-        const isErrorMessageVisible = await auth.isErrorMessageVisible();
+        const isSuccessful = await login.isLoginSuccessful();
+        const isErrorMessageVisible = await login.isErrorMessageVisible();
 
         expect(isSuccessful).toBe(false);
         expect(isErrorMessageVisible).toBe(true);
