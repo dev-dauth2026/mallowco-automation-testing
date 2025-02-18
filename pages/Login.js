ad4a99d6183f6d00864d244b03fd3f63fb006dc1
Dauth Gurung <dauth22grg22@gmail.com>
2025-02-18 12:28:39 +1000

    variable name updated

--- pages/Login.js
+++ pages/Login.js
@@ -5,39 +5,34 @@ export class Login extends BasePage {
   constructor(page) {
     super(page)
 
-    this.login_link =  page.getByRole('link', { name: 'Login', exact: true });
-    this.username_textbox = page.getByLabel("E-Mail Address *");
-    this.password_textbox = page.locator("#login_password");
-    this.login_button = page.getByRole("button", { name: "Login" });
-    this.login_error_message = page.getByText('These credentials do not match our records.', { exact: false });
-    this.forgot_your_password = page.getByRole('link', { name: 'Forgot Your Password?' });
-    this.facebook_login = page.locator('.login-facebook-btn');
-    this.google_login = page.locator('.login-google-btn');
-    this.signup_link = page.getByRole('link',{name:'Sign Up'});
+    this.loginLink =  page.getByRole('link', { name: 'Login', exact: true });
+    this.usernameTextbox = page.getByLabel("E-Mail Address *");
+    this.passwordTextbox = page.locator("#login_password");
+    this.loginButton = page.getByRole("button", { name: "Login" });
+    this.loginErrorMessage = page.getByText('These credentials do not match our records.', { exact: false });
+    this.forgotYourPassword = page.getByRole('link', { name: 'Forgot Your Password?' });
+    this.facebookLogin = page.locator('.login-facebook-btn');
+    this.googleLogin = page.locator('.login-google-btn');
+    this.signupLink = page.getByRole('link',{name:'Sign Up'});
 
   }
 
   async login(username, password) {
-    await this.login_link.click();
-    await this.username_textbox.fill(username);
-    await this.password_textbox.fill(password);
-    await this.login_button.click();
+    await this.loginLink.click();
+    await this.usernameTextbox.fill(username);
+    await this.passwordTextbox.fill(password);
+    await this.loginButton.click();
   }
 
   async getCurrentUrl(){
     return this.page.url();
   }
 
-  async isLoginSuccessful(){
-    return this.profile_link.isVisible();
-
-  }
-
   async isErrorMessageVisible(){
-    return this.login_error_message.isVisible();
+    return this.loginErrorMessage.isVisible();
   }
 
   async clickSignUpLink(){
-    await this.signup_link.click();
+    await this.signupLink.click();
   }
 }
