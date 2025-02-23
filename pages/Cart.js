b29d24e6105c9a4a4a9746909b0bb95ab3d8a45b
Dauth Gurung <dauth22grg22@gmail.com>
2025-02-23 15:19:25 +1000

    updated locators

--- pages/Cart.js
+++ pages/Cart.js
@@ -3,77 +3,23 @@ import { BasePage } from "./BasePage";
 export class Cart extends BasePage {
   constructor(page) {
     super(page)
-     
-    this.shop_link = page
-      .getByRole("banner")
-      .getByRole("link", { name: "Shop" });
-    this.product = page.locator(".product-header > a").first();
-    this.add_to_cart_button = page.getByRole("button", { name: "Add to cart" });
-    this.email_input = page.getByLabel("E-Mail Address *");
-    this.password_input = page.locator("#login_password");
-    this.login_button = page.getByRole("button", { name: "Login" });
-    this.cart_details_link = page.getByRole("link", {
-      name: "login image My Cart",
-    });
-    this.cart_close = page.getByRole("button", { name: "Close" });
-    this.cart_item = page.locator(".cart-item");
-  }
-
-  
-
-  async closePage() {
-    await this.page.close();
-  }
-
-
-  
-
-  async addToCart(email, password) {
-    // 1. Navigate and add a product
-    await this.shop_link.click();
-    await this.product.click();
-    await this.add_to_cart_button.click();
-
-    // 2. Login
-    await this.email_input.fill(email);
-    await this.password_input.fill(password);
-    await this.login_button.click();
-
-    // 3. Open cart & close it
-    await this.cart_details_link.click();
-    await this.cart_close.click();
-
-    // 4. Add product again (if desired) & open cart
-    await this.add_to_cart_button.click();
-    await this.cart_details_link.click();
-
-    // --- Debugging: Check initial count properly ---
-    await this.cart_item.last().waitFor({ state: "visible" });
-
-    await this.cart_item.
-
-    console.log('Total cart item', await this.cart_item.count());
-    await this.page.pause();
-    while ((await this.cart_item.count()) > 0) {
-      console.log("Current Count", await this.cart_item.count());
-
-      // Remove first cart item
-      await this.cart_item.locator(".js-remove-cart-item").first().click();
-
-      // Optionally wait for the first item to be detached or the cart to update
-      await this.cart_item
-        .first()
-        .waitFor({ state: "detached" })
-        .catch(() => {});
-
     
-    // If cart is empty, break
-    if ((await this.cart_item.count()) === 1) {
-        break;
-      }
-    }
+    //product card locators
+    this.cartItems = page.locator('.cart-list .cart-item .cart-text');
+    this.cartItemName = this.cartItems.locator('h4');
+    this.cartItemVendor = this.cartItems.locator('strong');
+    this.cartItemQuantity = this.cartItems.locator('input[name="product_quantity"]');
+    this.cartItemSize = this.cartItems.locator('.pckt-select');
+    this.cartItemPrice = this.cartItems.locator('.price strong');
+    this.cartItemRemoveButton = this.cartItems.locator('.cart-close-btn');
+    this.quantityDecrementButton = this.cartItems.locator('.minus');
+    this.quantityIncrementButton = this.cartItems.locator('.plus');
 
-    await this.cart_close.click();
-    await this.cart_details_link.click();
+    // Cart Summary locators
+    this.totalCartItemsPrice = page.locator('.total span');
+    this.checkoutButton = page.getByRole('link',{name:'Proceed to Checkout'});
+
   }
+
+
 };
