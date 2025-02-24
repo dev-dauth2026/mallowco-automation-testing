c28acd543ac8cd44fe7eb41d0434d414e26434be
Dauth Gurung <dauth22grg22@gmail.com>
2025-02-24 14:25:14 +1000

    getNumericQuantityAndPriceValue util method added for reusability

--- pages/utils/numericPriceUtil.js
+++ pages/utils/numericPriceUtil.js
@@ -5,3 +5,15 @@ export function getNumericPriceValue(priceString) {
     const numericValue = priceString.replace(/[^0-9.]/g, '').trim(); // Remove non-numeric characters except '.'
     return parseFloat(numericValue); // Convert to number
 }
+
+export function getNumericQuantityAndPriceValue(priceQuantityString) {
+    if (!priceQuantityString) return 0; // Handle empty or null input
+
+    const numericQuantityAndPriceArray = priceQuantityString.replace(/[^0-9. ]/g, '')
+                        .replace(/\s+/g, ' ')
+                        .trim().split(" "); // Remove non-numeric characters except '.'
+    const quantity =  parseFloat(numericQuantityAndPriceArray[0]); // Convert to number
+    const price =  parseFloat(numericQuantityAndPriceArray[1]); // Convert to number
+
+    return {quantity,price};
+}
